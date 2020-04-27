2d6bd8d27d79fd5fd42218fd700caecc5b82fae4
Moses A. Samuel <mosamuel98@gmail.com>
2020-04-27 15:17:22 -0700

    move sprites to the src folder; render character sprite

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Route } from 'react-router-dom'
-import Register from './Login/register'
-import Login from './Login/login'
-import Map from './Game/Map'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Route } from "react-router-dom";
+import Register from "./Login/register";
+import Login from "./Login/login";
+import Map from "./Game/Map";
+
+import { ReactComponent as CharacterBoyFaceLeft } from "./sprites/character_boy_faceLeft.svg";
 
 function App() {
-  return (
-    <>
-    <Route exact path="/register" component={Register}/>
-    <Route path="/login" component={Login} />
-    <Route path="/game" component={Map} />
-    </>
-  );
+	return (
+		<>
+			<CharacterBoyFaceLeft />
+			<svg></svg>
+			<Route exact path="/register" component={Register} />
+			<Route path="/login" component={Login} />
+			<Route path="/game" component={Map} />
+		</>
+	);
 }
 
 export default App;
