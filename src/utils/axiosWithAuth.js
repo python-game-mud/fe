1b74fc42f91f6d2b47b2c79777e02dfafbcddc2a
Brendan <brendanschatz97@gmail.com>
2020-04-27 13:41:22 -0400

    initial setup

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -5,7 +5,7 @@ const axiosWithAuth = () => {
   return axios.create({
     baseURL: 'https://lambda-mud-test.herokuapp.com/',
     headers: {
-      Authorization: {'token': "e1f5dd85f87b0312ee31b096abbe08890ee06e30"}
+      Authorization: {'token': token}
     },
   })
 }
