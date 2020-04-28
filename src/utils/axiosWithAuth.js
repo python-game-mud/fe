b6e85c0c11a1cff2b4caaea6db40f606b406d5be
Moses A. Samuel <mosamuel98@gmail.com>
2020-04-28 14:28:36 -0700

    remove cors anywhere from the axiosWithAuth baseURL
    Co-authored-by: Brendan Schatz <brendanschatz97@gmail.com>

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -4,7 +4,7 @@ const axiosWithAuth = () => {
 	const token = localStorage.getItem("the_mud_game_token");
 	return axios.create({
 		baseURL:
-			"https://cors-anywhere.herokuapp.com/" +
+			// "https://cors-anywhere.herokuapp.com/" +
 			"http://themudgame.herokuapp.com/",
 		headers: {
 			Authorization: token,
