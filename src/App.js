b0832f239ca4af9b9089478ff7a841b0601f1fbd
Moses A. Samuel <mosamuel98@gmail.com>
2020-04-30 13:51:12 -0700

    add a route for the home page; make the game route a protected route

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import { Route } from "react-router-dom";
 import Register from "./Login/Register";
 import Login from "./Login/Login";
 import Map from "./Game/Map";
+import Home from "./Home";
+import ProtectedRoute from "./ProtectedRoute";
 
 function App() {
 	return (
 		<>
-			<Route exact path="/" component={Login} />
+			<Route exact path="/" component={Home} />
+			<Route exact path="/login" component={Login} />
 			<Route exact path="/register" component={Register} />
-			<Route path="/game" component={Map} />
+			<Route path="/game" component={ProtectedRoute(Map)} />
 		</>
 	);
 }
