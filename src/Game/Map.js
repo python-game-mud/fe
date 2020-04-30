0f997f1c6350dc2bc4d09fc83629df8fd112b9e3
Moses A. Samuel <mosamuel98@gmail.com>
2020-04-30 15:55:27 -0700

    change the color of specific nodes
    Co-authored-by: Vincent Pagano <vpagano10@gmail.com>
    Co-authored-by: Joshua Hill <Josh_Hill15@me.com>

--- src/Game/Map.js
+++ src/Game/Map.js
@@ -5,15 +5,23 @@ import img from "./man.png";
 import Pusher from "./Pusher";
 import { Graph } from "react-d3-graph";
 
+const Maps = styled.div`
+	background: #282c34;
+	min-height: 100vh;
+	width: 80vw;
+`;
+
+const CenteredDiv = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	padding-left: 5%;
+`;
+
 export default function Map() {
 	const [rooms, setRooms] = useState([]);
 	const [players, setPlayers] = useState([]);
-
-	const Maps = styled.div`
-		background: #282c34;
-		min-height: 100vh;
-		width: 80vw;
-	`;
+	const [currentRoom, setCurrentRoom] = useState("1");
 
 	const graphData = {
 		nodes: mapRoomsToNodes(rooms),
@@ -24,7 +32,7 @@ export default function Map() {
 		nodeHighlightBehavior: true,
 		staticGraph: true,
 		node: {
-			color: "#ffdf00",
+			color: "orange",
 			size: 250,
 			highlightStrokeColor: "red",
 			fontSize: 0,
@@ -71,21 +79,40 @@ export default function Map() {
 			.catch(err => console.error(err.response));
 	}, []);
 
+	React.useLayoutEffect(
+		function highlightCurrentRoom() {
+			setTimeout(() => {
+				const allNodes = Array.from(document.querySelectorAll(".node"));
+				console.log("\n HERE", allNodes, "\n\n");
+
+				allNodes.forEach(node => {
+					// if (node.id === `${currentRoom}`) {
+					if (node.id === "25") {
+						node.viewportElement.style.fill = "red";
+						node.innerHTML =
+							"<path cursor='pointer' opacity='1' d='M0,-14.71415478191356L8.495221224235612,0L0,14.71415478191356L-8.495221224235612,0Z fill='blue' stroke='none' stroke-width='1.5'></path><text dx='4' dy='.35em' fill='black' font-size='0' font-weight='normal' opacity='1'>95</text>";
+					}
+				});
+			}, 1000);
+		},
+		[currentRoom]
+	);
+
 	return (
 		<Maps>
-			<div>
-				<Pusher />
+			<Pusher />
+			<CenteredDiv>
 				{rooms.length > 0 && (
 					<Graph
 						id="map"
 						data={graphData}
 						config={graphConfig}
 						onClickGraph={onClickGraph}
-						// width="500px"
-						// height="500px"
+						width={5000}
+						height={5000}
 					/>
 				)}
-			</div>
+			</CenteredDiv>
 		</Maps>
 	);
 }
@@ -108,7 +135,6 @@ export default function Map() {
  * @returns {Edge[]}
  */
 function createEdges(source, targets) {
-	console.log({ targets });
 	return targets.reduce(
 		(acc, t) => acc.concat({ source: source, target: t }),
 		[]
