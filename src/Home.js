c1b7451508a9bd14dc8ae9612dabf9a73dbfb4f5
Moses A. Samuel <mosamuel98@gmail.com>
2020-04-30 14:23:12 -0700

    style the home page
    Co-authored-by: Joshua Hill <Josh_Hill15@me.com>
    Co-authored-by: Vincent Pagano <vpagano10@gmail.com>

--- src/Home.js
+++ src/Home.js
@@ -3,30 +3,56 @@ import { Link } from "react-router-dom";
 import { getToken } from "./utils/axiosWithAuth";
 import styled from "styled-components";
 
+const Centered = styled.div`
+	background-color: #282c34;
+	width: 100%;
+	height: 100vh;
+	margin: auto;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+`;
+
 const BigRed = styled.div`
-	background: "red";
-	color: "white";
-	font-size: 5rem;
+	width: 400px;
+	height: 100px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background-color: red;
+	color: white;
+	border: 4px solid black;
+
+	button {
+		display: inline block;
+		width: 200px;
+		height: 50px;
+		border: 2px solid black;
+		background-color: #fff;
+		font-weight: bold;
+		font-size: 2rem;
+	}
 `;
 
 export default function Home() {
 	let signedIn = getToken();
 	return (
-		<>
-			<h2>
-				Welcom to the <strong>"BEST"</strong> MUD game
+		<Centered>
+			<h2 style={{ color: "#ffdf00" }}>
+				Welcome to the <strong>"BEST"</strong> MUD game
 			</h2>
 			<BigRed>
-				{signedIn ? (
-					<Link to="/game">
+				{!signedIn ? (
+					<Link to="/login">
 						<button>PLAY NOW</button>
 					</Link>
 				) : (
-					<Link to="/login">
+					<Link to="/game">
 						<button>PLAY NOW</button>
 					</Link>
 				)}
 			</BigRed>
-		</>
+		</Centered>
 	);
 }
