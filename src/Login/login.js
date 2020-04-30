2a7131d34b566a4c0777f3cf6ce2829981b3fc9a
Moses A. Samuel <mosamuel98@gmail.com>
2020-04-30 13:21:45 -0700

    remove loading UI
    Co-authored-by: Brendan Schatz <brendanschatz97@gmail.com>
    Co-authored-by: Vincent Pagano <vpagano10@gmail.com>
    Co-authored-by: Joshua Hill <Josh_Hill15@me.com>

--- src/Login/login.js
+++ src/Login/login.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styled from "styled-components";
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 import { ReactComponent as CharacterBoyFaceLeft } from "../sprites/character_boy_faceLeft.svg";
 
 export default function Register(props) {
@@ -40,16 +40,10 @@ export default function Register(props) {
 		border-radius: 10px;
 	`;
 
-	const Loading = styled.h1`
-		color: red;
-		margin: 0;
-		text-align: center;
-		font-size: 4rem;
-	`;
-
 	const handleChange = e => {
 		setUser({ ...user, [e.target.name]: e.target.value });
 	};
+
 	const handleSubmit = e => {
 		e.preventDefault();
 		axios
@@ -76,39 +70,26 @@ export default function Register(props) {
 
 	return (
 		<Centerer>
-			<Page
-				src={require("./jungle.mp4")}
-				autoPlay
-				onEnded={() => {
-					setLoading(false);
-				}}
-			/>
-			{loading == true ? (
-				<div>
-					<CharacterBoyFaceLeft width={"500px"} height={`500px`} />
-					<Loading> LOADING...</Loading>
-				</div>
-			) : (
-				<LoginArea>
-					<form onSubmit={handleSubmit}>
-						<h1>Username:</h1>
-						<input
-							name="username"
-							value={user.username}
-							onChange={handleChange}
-						></input>
-						<h1>Password:</h1>
-						<input
-							name="password"
-							type="password"
-							value={user.password}
-							onChange={handleChange}
-						></input>
-					</form>
-					<button onClick={handleSubmit}> LETS GOOOOOOO </button>
-					<h1 onClick={routeToRegistrationPage}>Don't have an account???</h1>
-				</LoginArea>
-			)}
+			<LoginArea>
+				<form onSubmit={handleSubmit}>
+					<h1>Username:</h1>
+					<input
+						name="username"
+						type="text"
+						value={user.username}
+						onChange={handleChange}
+					></input>
+					<h1>Password:</h1>
+					<input
+						name="password"
+						type="password"
+						value={user.password}
+						onChange={handleChange}
+					></input>
+				</form>
+				<button onClick={handleSubmit}> LETS GOOOOOOO </button>
+				<h1 onClick={routeToRegistrationPage}>Don't have an account???</h1>
+			</LoginArea>
 			{errors.length > 0 && (
 				<div>
 					{errors.map(e => (
@@ -116,6 +97,12 @@ export default function Register(props) {
 					))}
 				</div>
 			)}
+			<div>
+				<p>Don't have an account yet?</p>
+				<p>
+					Click <Link to="/register">here</Link> to register.
+				</p>
+			</div>
 		</Centerer>
 	);
 }
