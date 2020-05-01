057c8fab604c5648a42f6e2f9d848da5624bf7e1
vpagano10 <vpagano10@gmail.com>
2020-05-01 09:01:04 -0400

    Cleaning up map page for easier room movement and graph visability.

--- src/Game/Map.js
+++ src/Game/Map.js
@@ -6,106 +6,107 @@ import Pusher from "./Pusher";
 import { CurrentRoomCtx } from "../App";
 
 const Maps = styled.div`
-	background: #282c34;
-	min-height: 100vh;
-	width: 80vw;
+  background: #282c34;
+  min-height: 100vh;
+  width: 80vw;
 `;
 
 const CenteredDiv = styled.div`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	padding-left: 5%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 5%;
+  padding-left: 5%;
 `;
 
 export default function Map() {
-	const [rooms, setRooms] = useState([]);
-	const [players, setPlayers] = useState([]);
-	const { currentRoom } = useContext(CurrentRoomCtx);
+  const [rooms, setRooms] = useState([]);
+  const [players, setPlayers] = useState([]);
+  const { currentRoom } = useContext(CurrentRoomCtx);
 
-	const graphData = {
-		nodes: mapRoomsToNodes(rooms),
-		links: flattenEdges(mapRoomsToEdges(rooms)),
-	};
+  const graphData = {
+    nodes: mapRoomsToNodes(rooms),
+    links: flattenEdges(mapRoomsToEdges(rooms)),
+  };
 
-	const graphConfig = {
-		nodeHighlightBehavior: true,
-		staticGraph: true,
-		node: {
-			color: "orange",
-			size: 250,
-			highlightStrokeColor: "red",
-			fontSize: 0,
-			rendderLabel: false,
-			symbolType: "diamond",
-		},
-		link: { highlightColor: "lightblue" },
-	};
+  const graphConfig = {
+    nodeHighlightBehavior: true,
+    staticGraph: true,
+    node: {
+      color: "orange",
+      size: 250,
+      highlightStrokeColor: "red",
+      fontSize: 0,
+      rendderLabel: false,
+      symbolType: "diamond",
+    },
+    link: { highlightColor: "lightblue" },
+  };
 
-	const onClickGraph = () => {
-		// TODO: expand/shrink the graph
-		console.log("graph clicked");
-	};
+  const onClickGraph = () => {
+    // TODO: expand/shrink the graph
+    console.log("graph clicked");
+  };
 
-	useEffect(function fetchData() {
-		axiosWithAuth()
-			.get("api/adv/rooms/")
-			.then(res => {
-				console.log("Rooms:", res.data);
-				Array.isArray(res.data) &&
-					setRooms(
-						res.data.map(
-							({ id, title, description, n_to, s_to, e_to, w_to }) => ({
-								id,
-								title,
-								description,
-								n_to,
-								s_to,
-								e_to,
-								w_to,
-							})
-						)
-					);
-				Array.isArray(res.data) &&
-					setPlayers(res.data.map(({ players }) => ({ players })));
-			})
-			.catch(err => console.error(err.response));
-	}, []);
+  useEffect(function fetchData() {
+    axiosWithAuth()
+      .get("api/adv/rooms/")
+      .then((res) => {
+        console.log("Rooms:", res.data);
+        Array.isArray(res.data) &&
+          setRooms(
+            res.data.map(
+              ({ id, title, description, n_to, s_to, e_to, w_to }) => ({
+                id,
+                title,
+                description,
+                n_to,
+                s_to,
+                e_to,
+                w_to,
+              })
+            )
+          );
+        Array.isArray(res.data) &&
+          setPlayers(res.data.map(({ players }) => ({ players })));
+      })
+      .catch((err) => console.error(err.response));
+  }, []);
 
-	React.useLayoutEffect(
-		function highlightCurrentRoom() {
-			setTimeout(() => {
-				const allNodes = Array.from(document.querySelectorAll(".node"));
+  React.useLayoutEffect(
+    function highlightCurrentRoom() {
+      setTimeout(() => {
+        const allNodes = Array.from(document.querySelectorAll(".node"));
 
-				allNodes.forEach(node => {
-					if (node.id === `${currentRoom}`) {
-						node.viewportElement.style.fill = "red";
-						node.innerHTML =
-							"<path cursor='pointer' opacity='1' d='M0,-14.71415478191356L8.495221224235612,0L0,14.71415478191356L-8.495221224235612,0Z fill='blue' stroke='none' stroke-width='1.5'></path><text dx='4' dy='.35em' fill='black' font-size='0' font-weight='normal' opacity='1'>95</text>";
-					}
-				});
-			}, 1000);
-		},
-		[currentRoom]
-	);
+        allNodes.forEach((node) => {
+          if (node.id === `${currentRoom}`) {
+            node.viewportElement.style.fill = "red";
+            node.innerHTML =
+              "<path cursor='pointer' opacity='1' d='M0,-14.71415478191356L8.495221224235612,0L0,14.71415478191356L-8.495221224235612,0Z fill='blue' stroke='none' stroke-width='1.5'></path><text dx='4' dy='.35em' fill='black' font-size='0' font-weight='normal' opacity='1'>95</text>";
+          }
+        });
+      }, 1000);
+    },
+    [currentRoom]
+  );
 
-	return (
-		<Maps>
-			<Pusher />
-			<CenteredDiv>
-				{rooms.length > 0 && (
-					<Graph
-						id="map"
-						data={graphData}
-						config={graphConfig}
-						onClickGraph={onClickGraph}
-						width={5000}
-						height={5000}
-					/>
-				)}
-			</CenteredDiv>
-		</Maps>
-	);
+  return (
+    <Maps>
+      <Pusher />
+      <CenteredDiv>
+        {rooms.length > 0 && (
+          <Graph
+            id="map"
+            data={graphData}
+            config={graphConfig}
+            onClickGraph={onClickGraph}
+            width={5000}
+            height={5000}
+          />
+        )}
+      </CenteredDiv>
+    </Maps>
+  );
 }
 
 /**
@@ -126,10 +127,10 @@ export default function Map() {
  * @returns {Edge[]}
  */
 function createEdges(source, targets) {
-	return targets.reduce(
-		(acc, t) => acc.concat({ source: source, target: t }),
-		[]
-	);
+  return targets.reduce(
+    (acc, t) => acc.concat({ source: source, target: t }),
+    []
+  );
 }
 
 //
@@ -139,12 +140,12 @@ function createEdges(source, targets) {
  * @returns {Edge[][]}
  */
 function mapRoomsToEdges(rooms) {
-	return rooms.map(room => {
-		const nonEmptyRooms = [room.n_to, room.s_to, room.e_to, room.w_to].filter(
-			direction => parseInt(direction) !== 0
-		);
-		return createEdges(room.id, nonEmptyRooms);
-	});
+  return rooms.map((room) => {
+    const nonEmptyRooms = [room.n_to, room.s_to, room.e_to, room.w_to].filter(
+      (direction) => parseInt(direction) !== 0
+    );
+    return createEdges(room.id, nonEmptyRooms);
+  });
 }
 
 /**
@@ -153,7 +154,7 @@ function mapRoomsToEdges(rooms) {
  * @returns {Edge[]}
  */
 function flattenEdges(edges) {
-	return edges.flat();
+  return edges.flat();
 }
 
 /**
@@ -169,9 +170,9 @@ function flattenEdges(edges) {
  */
 
 function mapRoomsToNodes(rooms) {
-	return rooms.map(room => ({
-		id: room.id,
-		x: 30 * Math.round(room.id % 15),
-		y: 30 * Math.round(room.id / 15),
-	}));
+  return rooms.map((room) => ({
+    id: room.id,
+    x: 30 * Math.round(room.id % 15),
+    y: 30 * Math.round(room.id / 15),
+  }));
 }
--- src/Game/Pusher.js
+++ src/Game/Pusher.js
@@ -12,280 +12,280 @@ import { ReactComponent as Doors } from "../sprites/door.svg";
 import { ReactComponent as Doors2 } from "../sprites/door2.svg";
 
 const SideBar = styled.div`
-	width: 20vw;
-	height: 100vh;
-	background-color: red;
-	position: absolute;
-	right: 0;
-	display: flex;
-	flex-direction: column;
-	justify-content: flex-end;
+  width: 20vw;
+  height: 100vh;
+  background-color: red;
+  position: absolute;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
 `;
 const ButtonHolder = styled.div`
-	background: black;
-	width: 80%;
-	height: 30%;
-	position: absolute;
-	left: 10%;
-	top: 10%;
+  background: black;
+  width: 80%;
+  height: 30%;
+  position: absolute;
+  left: 10%;
+  top: 10%;
 `;
 
 const Right = styled.div`
-	position: relative;
-	left: 56%;
-	top: 8%;
-	border-color: transparent purple;
-	border-style: solid;
-	border-width: 40px 0px 40px 80px;
-	height: 0px;
-	width: 0px;
+  position: relative;
+  left: 56%;
+  top: 8%;
+  border-color: transparent purple;
+  border-style: solid;
+  border-width: 40px 0px 40px 80px;
+  height: 0px;
+  width: 0px;
 `;
 
 const Left = styled.div`
-	position: relative;
-	left: 5%;
-	top: 28%;
-	border-color: transparent purple;
-	border-style: solid;
-	border-width: 40px 80px 40px 0px;
-	height: 0px;
-	width: 0px;
+  position: relative;
+  left: 5%;
+  top: 28%;
+  border-color: transparent purple;
+  border-style: solid;
+  border-width: 40px 80px 40px 0px;
+  height: 0px;
+  width: 0px;
 `;
 const Up = styled.div`
-	position: relative;
-	left: 32.5%;
-	top: -35%;
-	border-color: purple transparent;
-	border-style: solid;
-	border-width: 0px 40px 70px 40px;
-	height: 0px;
-	width: 0px;
+  position: relative;
+  left: 32.5%;
+  top: -35%;
+  border-color: purple transparent;
+  border-style: solid;
+  border-width: 0px 40px 70px 40px;
+  height: 0px;
+  width: 0px;
 `;
 
 const Down = styled.div`
-	position: relative;
-	left: 32.5%;
-	top: -5%;
-	border-color: purple transparent;
-	border-style: solid;
-	border-width: 70px 40px 0px 40px;
-	height: 0px;
-	width: 0px;
+  position: relative;
+  left: 32.5%;
+  top: -5%;
+  border-color: purple transparent;
+  border-style: solid;
+  border-width: 70px 40px 0px 40px;
+  height: 0px;
+  width: 0px;
 `;
 
 const Room = styled.div`
-	position: fixed;
-	width: 60%;
-	height: 60%;
-	background-color: green;
-	top: 20%;
-	left: 10%;
-	border: 30px solid brown;
+  position: fixed;
+  width: 60%;
+  height: 60%;
+  background-color: green;
+  top: 30%;
+  left: 10%;
+  border: 30px solid brown;
 `;
 
 const Door = styled.div`
-	position: fixed;
-	left: 60%;
-	top: 30%;
+  position: fixed;
+  left: 60%;
+  top: 40%;
 `;
 const EastDoor = styled.div`
-	position: fixed;
-	left: 0%;
-	top: 30%;
+  position: fixed;
+  left: 0%;
+  top: 40%;
 `;
 const NorthDoor = styled.div`
-	position: fixed;
-	left: 30%;
-	top: 0%;
+  position: fixed;
+  left: 30%;
+  top: 0%;
 `;
 
 export default function Pusher(props) {
-	const [left, setLeft] = useState(10);
-	const [top, setTop] = useState(10);
-	const [doorTop, setDoorTop] = useState(false);
-	const [doorLeft, setDoorLeft] = useState(10);
-	const [info, setInfo] = useState("");
-	const [characterDirection, setCharacterDirection] = useState("right");
+  const [left, setLeft] = useState(30);
+  const [top, setTop] = useState(30);
+  const [doorTop, setDoorTop] = useState(false);
+  const [doorLeft, setDoorLeft] = useState(10);
+  const [info, setInfo] = useState("");
+  const [characterDirection, setCharacterDirection] = useState("right");
 
-	const { setCurrentRoom } = useContext(CurrentRoomCtx);
+  const { setCurrentRoom } = useContext(CurrentRoomCtx);
 
-	const Character = styled.div`
-		position: absolute;
-		left: ${left}%;
-		top: ${top}%;
-	`;
+  const Character = styled.div`
+    position: absolute;
+    left: ${left}%;
+    top: ${top}%;
+  `;
 
-	useEffect(
-		function fetchData() {
-			if (info.title !== undefined) {
-				if (info.title.charAt(6) == 9 || info.title.charAt(7) == 9) {
-					setDoorTop(true);
-				} else {
-					setDoorTop(false);
-				}
-			}
+  useEffect(
+    function fetchData() {
+      if (info.title !== undefined) {
+        if (info.title.charAt(6) == 9 || info.title.charAt(7) == 9) {
+          setDoorTop(true);
+        } else {
+          setDoorTop(false);
+        }
+      }
 
-			axiosWithAuth()
-				.get("api/adv/init/")
-				.then(res => {
-					console.log("first init", res);
-					setInfo(res.data);
-					setCurrentRoom(res.data.title.split(" ")[1]);
-				})
-				.catch(err => {
-					console.log(err.response);
-				});
-		},
-		[left, top]
-	);
+      axiosWithAuth()
+        .get("api/adv/init/")
+        .then((res) => {
+          console.log("first init", res);
+          setInfo(res.data);
+          setCurrentRoom(res.data.title.split(" ")[1]);
+        })
+        .catch((err) => {
+          console.log(err.response);
+        });
+    },
+    [left, top]
+  );
 
-	function handleEast(res) {
-		if (res.data.error_msg == "") {
-			setLeft(10);
-			setInfo(res);
-			console.log("info", info);
-		} else {
-			setInfo(res);
-			setLeft(60);
-			console.log("error", info.error_msg);
-		}
-	}
-	function handleWest(res) {
-		if (res.data.error_msg == "") {
-			setLeft(60);
-			setInfo(res);
-			console.log("info", info);
-		} else {
-			setInfo(res);
-			setLeft(10);
-			console.log("error", info.error_msg);
-		}
-	}
-	function handleNorth(res) {
-		if (res.data.error_msg == "") {
-			setTop(60);
-			setInfo(res);
-			console.log("info", info);
-		} else {
-			setInfo(res);
-			setTop(10);
-			console.log("error", info.error_msg);
-		}
-	}
-	function handleSouth(res) {
-		if (res.data.error_msg == "") {
-			setTop(10);
-			setInfo(res);
-			console.log("info", info);
-		} else {
-			setInfo(res);
-			setTop(60);
-			console.log("error", info.error_msg);
-		}
-	}
-	if (left > 60) {
-		axiosWithAuth()
-			.post("/api/adv/move", { direction: "e" })
-			.then(res => {
-				handleEast(res);
-			})
-			.catch(err => {
-				console.log(err);
-			});
-	}
-	if (left < 10) {
-		axiosWithAuth()
-			.post("/api/adv/move", { direction: "w" })
-			.then(res => {
-				handleWest(res);
-			})
-			.catch(err => {
-				console.log(err);
-			});
-	}
-	if (top < 10) {
-		axiosWithAuth()
-			.post("/api/adv/move", { direction: "n" })
-			.then(res => {
-				handleNorth(res);
-			})
-			.catch(err => {
-				console.log(err);
-			});
-	}
-	if (top > 60) {
-		axiosWithAuth()
-			.post("/api/adv/move", { direction: "s" })
-			.then(res => {
-				handleSouth(res);
-			})
-			.catch(err => {
-				console.log(err);
-			});
-	}
-	const moveLeft = () => {
-		setCharacterDirection("left");
-		setLeft(left - 10);
-		// console.log("LEFT:", left);
-	};
+  function handleEast(res) {
+    if (res.data.error_msg == "") {
+      setLeft(10);
+      setInfo(res);
+      console.log("info", info);
+    } else {
+      setInfo(res);
+      setLeft(60);
+      console.log("error", info.error_msg);
+    }
+  }
+  function handleWest(res) {
+    if (res.data.error_msg == "") {
+      setLeft(60);
+      setInfo(res);
+      console.log("info", info);
+    } else {
+      setInfo(res);
+      setLeft(10);
+      console.log("error", info.error_msg);
+    }
+  }
+  function handleNorth(res) {
+    if (res.data.error_msg == "") {
+      setTop(60);
+      setInfo(res);
+      console.log("info", info);
+    } else {
+      setInfo(res);
+      setTop(10);
+      console.log("error", info.error_msg);
+    }
+  }
+  function handleSouth(res) {
+    if (res.data.error_msg == "") {
+      setTop(10);
+      setInfo(res);
+      console.log("info", info);
+    } else {
+      setInfo(res);
+      setTop(60);
+      console.log("error", info.error_msg);
+    }
+  }
+  if (left > 60) {
+    axiosWithAuth()
+      .post("/api/adv/move", { direction: "e" })
+      .then((res) => {
+        handleEast(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  if (left < 10) {
+    axiosWithAuth()
+      .post("/api/adv/move", { direction: "w" })
+      .then((res) => {
+        handleWest(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  if (top < 10) {
+    axiosWithAuth()
+      .post("/api/adv/move", { direction: "n" })
+      .then((res) => {
+        handleNorth(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  if (top > 60) {
+    axiosWithAuth()
+      .post("/api/adv/move", { direction: "s" })
+      .then((res) => {
+        handleSouth(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  const moveLeft = () => {
+    setCharacterDirection("left");
+    setLeft(left - 10);
+    // console.log("LEFT:", left);
+  };
 
-	const moveRight = () => {
-		setCharacterDirection("right");
-		setLeft(left + 10);
-		// console.log("LEFT:", left);
-	};
+  const moveRight = () => {
+    setCharacterDirection("right");
+    setLeft(left + 10);
+    // console.log("LEFT:", left);
+  };
 
-	const moveUp = () => {
-		setTop(top - 10);
-		// console.log("TOP:", top);
-	};
+  const moveUp = () => {
+    setTop(top - 10);
+    // console.log("TOP:", top);
+  };
 
-	const moveDown = () => {
-		setTop(top + 10);
-		// console.log("TOP:", top);
-	};
-	return (
-		<>
-			<SideBar>
-				<div>
-					<Link to="/logout">
-						<button>logout</button>
-					</Link>
-				</div>
-				<ButtonHolder>
-					<Left onClick={moveLeft}></Left>
-					<Right onClick={moveRight}> </Right>
-					<Up onClick={moveUp}> </Up>
-					<Down onClick={moveDown}></Down>
-				</ButtonHolder>
+  const moveDown = () => {
+    setTop(top + 10);
+    // console.log("TOP:", top);
+  };
+  return (
+    <>
+      <SideBar>
+        <div>
+          <Link to="/logout">
+            <button>logout</button>
+          </Link>
+        </div>
+        <ButtonHolder>
+          <Left onClick={moveLeft}></Left>
+          <Right onClick={moveRight}> </Right>
+          <Up onClick={moveUp}> </Up>
+          <Down onClick={moveDown}></Down>
+        </ButtonHolder>
 
-				{/* <Chat /> */}
-				<div>
-					<h1>{info.name}</h1>
-					<h1>{info.title}</h1>
-				</div>
-			</SideBar>
-			<Room />
-			{setDoorTop == false ? (
-				<NorthDoor>
-					<Doors height="500px" width="500px" />
-				</NorthDoor>
-			) : null}
+        {/* <Chat /> */}
+        <div>
+          <h1>{info.name}</h1>
+          <h1>{info.title}</h1>
+        </div>
+      </SideBar>
+      <Room />
+      {setDoorTop == false ? (
+        <NorthDoor>
+          <Doors height="500px" width="500px" />
+        </NorthDoor>
+      ) : null}
 
-			<Door>
-				<Doors2 height="500px" width="500px" />
-			</Door>
-			<EastDoor>
-				<Doors2 height="500px" width="500px" />
-			</EastDoor>
+      <Door>
+        <Doors2 height="500px" width="500px" />
+      </Door>
+      <EastDoor>
+        <Doors2 height="500px" width="500px" />
+      </EastDoor>
 
-			<Character>
-				{characterDirection.toLowerCase() === "left" ? (
-					<CharacterBoyFaceLeft height="300px" width="300px" />
-				) : (
-					<CharacterBoyFaceRight height="300px" width="300px" />
-				)}
-			</Character>
-		</>
-	);
+      <Character>
+        {characterDirection.toLowerCase() === "left" ? (
+          <CharacterBoyFaceLeft height="300px" width="300px" />
+        ) : (
+          <CharacterBoyFaceRight height="300px" width="300px" />
+        )}
+      </Character>
+    </>
+  );
 }
